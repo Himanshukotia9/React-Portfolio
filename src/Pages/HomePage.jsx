50149f3453055caa6149389d44c0c92f3a2babbc
Himanshu kotia <himanshukotia9@gmail.com>
2024-07-26 17:35:37 +0530

    download button error solved

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -28,7 +28,7 @@ export default function HomePage() {
                 initial="hidden" animate="visible"  className='py-2 text-slate-500 md:text-lg'>{HOME_TEXT}</motion.p>
                 <motion.div variants={container(0.9)}
                 initial="hidden" animate="visible" id="btn-box" className='my-5'>
-                    <a href="public/Himanshu Kotia Resume.pdf" download="" id="btn" className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 transition-all'>Download CV</a>
+                    <a href="/Himanshu Kotia Resume.pdf" download="" id="btn" className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 transition-all'>Download CV</a>
                     <Link to='/contact' className='text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800 transition-all' id="btn">Let's Talk</Link>
                 </motion.div>
             </div>  
