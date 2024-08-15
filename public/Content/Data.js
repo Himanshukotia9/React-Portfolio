f85525137c25b0d0ecc061bd4346ede6e6e52d77
Himanshu kotia <himanshukotia9@gmail.com>
2024-08-15 13:02:56 +0530

    small changes in projects section

--- public/Content/Data.js
+++ public/Content/Data.js
@@ -1,11 +1,12 @@
-import project1 from "/Images/Projects/Prime-Insight.jpg";
-import project2 from "/Images/Projects/Typo-Mania.jpg";
-import project3 from "/Images/Projects/Chore-Champ.jpg";
-import project4 from "/Images/Projects/Coin-Counter.jpg";
-import project5 from "/Images/Projects/Match-Mastery.jpg";
-import project6 from "/Images/Projects/Four-Mation.jpg";
-import project7 from "/Images/Projects/Ecommerce-Landing-Page.jpg";
-import project8 from "/Images/Projects/Score-Keeper.jpg";
+import project1 from "/Images/Projects/Coin-Counter.jpg";
+import project2 from "/Images/Projects/Bmi-Calculator.jpg";
+import project3 from "/Images/Projects/Prime-Insight.jpg";
+import project4 from "/Images/Projects/Typo-Mania.jpg";
+import project5 from "/Images/Projects/Chore-Champ.jpg";
+import project6 from "/Images/Projects/Match-Mastery.jpg";
+import project7 from "/Images/Projects/Four-Mation.jpg";
+import project8 from "/Images/Projects/Ecommerce-Landing-Page.jpg";
+import project9 from "/Images/Projects/Score-Keeper.jpg";
 
 export const HOME_TEXT = `Welcome to my portfolio website! I am thrilled to showcase my skills and projects as a frontend developer. With a passion for creating visually appealing and user-friendly websites, I strive to bring innovative ideas to life through clean and efficient code. From crafting responsive layouts to implementing interactive features, I am constantly pushing the boundaries of web development. Through this portfolio, I invite you to explore my work and witness my dedication to delivering exceptional digital experiences. Join me on this journey as we embark on the exciting world of frontend development together. Let's create something remarkable!`;
 
@@ -13,8 +14,24 @@ export const ABOUT_TEXT = `As a front-end web developer, I have worked as a inte
 
 export const PROJECTS = [
   {
-    title: "Prime-Insight",
+    title: "Coin Counter",
     image: project1,
+    link: 'https://coin-counter-sigma.vercel.app/',
+    description:
+      "Coin Counter is a basic expenses tracking app. It provides a simple and efficient way to track your daily expenses. Users can input various expenditures, categorize them, and monitor their spending habits over time. With its easy-to-use interface, this app helps individuals manage their finances effectively, ensuring they stay within budget and make informed financial decisions.",
+    technologies: ["React", "React Hooks", "HTML", "CSS", "TailwindCSS", "JavaScript"],
+  },
+  {
+    title: "Body-Balance",
+    image: project2,
+    link: 'https://body-balance.vercel.app/',
+    description:
+      "Coin Counter is a basic expenses tracking app. It provides a simple and efficient way to track your daily expenses. Users can input various expenditures, categorize them, and monitor their spending habits over time. With its easy-to-use interface, this app helps individuals manage their finances effectively, ensuring they stay within budget and make informed financial decisions.",
+    technologies: ["React", "React Hooks", "HTML", "CSS", "TailwindCSS", "JavaScript"],
+  },
+  {
+    title: "Prime-Insight",
+    image: project3,
     link: 'https://prime-insight.netlify.app/',
     description:
       " This website offers insightful analysis and data-driven perspectives on various topics, aiming to provide users with valuable information and a deeper understanding of current trends and issues.",
@@ -22,7 +39,7 @@ export const PROJECTS = [
   },
   {
     title: "Typo-Mania",
-    image: project2,
+    image: project4,
     link: 'https://typo-mania.netlify.app/',
     description:
       "This website offers an engaging typing speed game designed to test and improve users' typing skills. The platform provides a fun and interactive way for users to practice typing accuracy and speed, making it a valuable tool for anyone looking to enhance their typing proficiency.",
@@ -30,23 +47,15 @@ export const PROJECTS = [
   },
   {
     title: "Chore Champ",
-    image: project3,
+    image: project5,
     link: 'https://chore-champ.netlify.app/',
     description:
       "This website is designed to help users manage and organize their daily tasks and chores efficiently. By providing a user-friendly interface and useful features, Chore Champ aims to make task management simple and effective, ensuring users can stay on top of their responsibilities and improve productivity.",
     technologies: ["HTML", "CSS", "TailwindCSS", "JavaScript"],
   },
   {
-    title: "Coin Counter",
-    image: project4,
-    link: 'https://coin-counter.netlify.app/',
-    description:
-      "Coin Counter is a basic expenses tracking app. It provides a simple and efficient way to track your daily expenses. Users can input various expenditures, categorize them, and monitor their spending habits over time. With its easy-to-use interface, this app helps individuals manage their finances effectively, ensuring they stay within budget and make informed financial decisions.",
-    technologies: ["HTML", "CSS", "TailwindCSS", "JavaScript"],
-  },
-  {
     title: "Match Mastery",
-    image: project5,
+    image: project6,
     link: 'https://match-mastery.netlify.app/',
     description:
       "This engaging web-based game challenges users to test and improve their memory by finding matching pairs of cards. The game features a sleek and interactive design, making it both fun and educational. Perfect for users of all ages, Match Mastery aims to enhance cognitive skills through enjoyable gameplay.",
@@ -54,7 +63,7 @@ export const PROJECTS = [
   },
   {
     title: "Four-Mation",
-    image: project6,
+    image: project7,
     link: 'https://four-mation.netlify.app/',
     description:
       "This website features a classic strategy game where players aim to connect four pieces in a row. With its clean and interactive design, Four-Mation offers a fun and challenging experience for players of all ages, enhancing strategic thinking and planning skills.",
@@ -62,18 +71,18 @@ export const PROJECTS = [
   },
   {
     title: "The eCommerce Responsive Landing Page",
-    image: project7,
+    image: project8,
     link: 'https://ecommerce-responsive-landingpage.netlify.app/',
     description:
       "This website showcases a sleek and modern landing page designed for eCommerce businesses. It features a responsive layout, ensuring optimal viewing on various devices, and includes essential elements such as product displays, promotional sections, and call-to-action buttons to enhance user engagement and drive sales.",
-    technologies: ["HTML", "CSS", "TailwindCSS", "JavaScript"],
+    technologies: ["HTML", "CSS", "TailwindCSS"],
   },
   {
     title: "Score Keeper",
-    image: project8,
+    image: project9,
     link: 'https://himanshukotia9.github.io/Score-keeper/Score-keeper.html',
     description:
       "Score Keeper is a simple and intuitive web application designed to help users track and manage scores for various games or activities. With a clean and user-friendly interface, this tool allows you to easily input, update, and view scores in real-time. Ideal for sports events, board games, or any activity where score tracking is needed, the Score Keeper ensures you never lose track of your progress and can focus on enjoying the game.",
-    technologies: ["HTML", "CSS", "TailwindCSS", "JavaScript"],
+    technologies: ["HTML", "CSS", "JavaScript"],
   },
 ];
